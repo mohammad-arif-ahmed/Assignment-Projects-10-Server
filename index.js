e380f0df90ab24350d0244fc71da51445f361720
Md.Arif Ahmed <mohammad.arif.ahmed.08@gmail.com>
2026-05-10 12:18:33 +0600

    delete course api implemented

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -45,6 +45,16 @@ async function run() {
             const newCourse = req.body;
 
             const result = await coursesCollection.insertOne(newCourse);
+
+            res.send(result);
+        });
+        app.delete("/courses/:id", async (req, res) => {
+
+            const id = req.params.id;
+
+            const query = { _id: new ObjectId(id) };
+
+            const result = await coursesCollection.deleteOne(query);
 
             res.send(result);
         });
